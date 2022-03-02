b591ca893f59df35c37173e901b45fd33ecb4de5
EetuHernesniemi <eetu.hernesniemi@hotmail.com>
2022-03-02 15:51:47 +0200

    messageCell fix

--- Flash Chat/Controllers/ChatViewController.swift
+++ Flash Chat/Controllers/ChatViewController.swift
@@ -24,8 +24,7 @@ class ChatViewController: UIViewController {
         tableView.dataSource = self
         title = K.appName
         navigationItem.hidesBackButton = true
-        tableView.register(UINib(nibName: K.cellNibName, bundle: nil),
-                           forCellReuseIdentifier: K.cellIdentifier)
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil),forCellReuseIdentifier: K.cellIdentifier)
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
@@ -45,8 +44,7 @@ extension ChatViewController: UITableViewDataSource {
         return messages.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
-        as! MessageCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
         let message = messages[indexPath.row]
         cell.label.text = message.body
         return cell
