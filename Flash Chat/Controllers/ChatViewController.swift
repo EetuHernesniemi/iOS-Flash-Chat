41b8f29cf8da8471ea79eb4b16079d55df244f45
EetuHernesniemi <eetu.hernesniemi@hotmail.com>
2022-03-02 13:02:31 +0200

    message table draft. click print function.

--- Flash Chat/Controllers/ChatViewController.swift
+++ Flash Chat/Controllers/ChatViewController.swift
@@ -13,8 +13,16 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    var messages: [Message] = [
+        Message(sender: "1@2.com", body: "Hey!"),
+        Message(sender: "a@b.com", body: "Hello!"),
+        Message(sender: "1@2.com", body: "how are youu doing?"),
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
         title = K.appName
         navigationItem.hidesBackButton = true
     }
@@ -29,6 +37,23 @@ class ChatViewController: UIViewController {
         } catch let signOutError as NSError {
           print("Error signing out: %@", signOutError)
         }
+    }
+}
+extension ChatViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
+        let message = messages[indexPath.row]
+        cell.textLabel?.text = message.body
+        return cell
+    }
+}
+
+extension ChatViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("this is row \((indexPath.row + 1)).")
     }
 }
 
---
+++ Flash Chat/Message.swift
@@ -0,0 +1,13 @@
+//
+//  Message.swift
+//  Flash Chat
+//
+//  Created by Eetu Hernesniemi on 28.2.2022.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
